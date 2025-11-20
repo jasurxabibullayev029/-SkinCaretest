143fa1a73abb7f735808abbbe5d2a7aa6bb075fa
Jasur_dev <157604330+jasurxabibullayev029@users.noreply.github.com>
2025-11-20 20:51:25 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ This file helps Gunicorn locate the Django WSGI application.
 """
 
 import os
-from django.core.wsgi import get_wsgi_application
+import sys
+from pathlib import Path
+
+# Add the project directory to the Python path
+project_root = str(Path(__file__).resolve().parent)
+if project_root not in sys.path:
+    sys.path.append(project_root)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skincare_ai.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
