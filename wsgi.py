2ff7635034d3253d4687305bba315d1e21586b45
Jasur_dev <157604330+jasurxabibullayev029@users.noreply.github.com>
2025-11-20 20:09:29 +0500

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+""
+WSGI config for skincare_ai project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skincare_ai.settings.production')
+
+application = get_wsgi_application()
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+application.add_files(settings.STATICFILES_DIRS, prefix='static/')
