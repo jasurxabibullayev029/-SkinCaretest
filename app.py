8aac112c4f6a3c27f0f0e6414cc8b0240cf67a1e
Jasur_dev <157604330+jasurxabibullayev029@users.noreply.github.com>
2025-11-21 15:22:05 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ sys.path.insert(0, str(BASE_DIR))
 sys.path.insert(0, os.path.join(BASE_DIR, 'skincare_ai'))
 
 # Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SkinCareProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skincare.settings')
 
 # This application object is used by any WSGI server configured to use this file.
 from django.core.wsgi import get_wsgi_application
