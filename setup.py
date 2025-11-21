bdefeab6a14c20b53cf907538fe11a3025aac8e7
Jasur_dev <157604330+jasurxabibullayev029@users.noreply.github.com>
2025-11-21 11:14:54 +0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 from setuptools import setup, find_packages
 import os
 
-# Read requirements from requirements.txt
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+# List of dependencies
+INSTALL_REQUIRES = [
+    'Django==5.2.7',
+    'djangorestframework==3.16.1',
+    'django-cors-headers==4.9.0',
+    'python-dotenv==1.1.1',
+    'pillow==12.0.0',
+    'requests==2.32.5',
+    'gunicorn==22.0.0',
+    'sqlparse==0.5.3',
+    'tzdata==2025.2',
+    'openai==2.6.1'
+]
 
 setup(
     name="skincare_ai",
     version="1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=INSTALL_REQUIRES,
     python_requires='>=3.8',
     package_data={
         '': ['*.html', '*.css', '*.js', '*.png', '*.jpg', '*.jpeg', '*.gif', '*.svg', '*.ico', '*.json'],
