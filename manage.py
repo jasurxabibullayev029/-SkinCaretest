fa5ad404a9305d52e2c9245a961cbdd6ae881c54
Jasur_dev <157604330+jasurxabibullayev029@users.noreply.github.com>
2025-11-23 18:29:52 +0500

    Add files via upload

--- manage.py
+++ manage.py
@@ -2,16 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from pathlib import Path
+
 
 def main():
     """Run administrative tasks."""
-    # Add the project directory to Python path
-    BASE_DIR = Path(__file__).resolve().parent
-    sys.path.append(str(BASE_DIR))
-    
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skincare.settings')
-    
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skincare_ai.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -20,8 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
