ccb0474ea6f9c9d5018f25e0d6389702d6db6182
Jasur_dev <157604330+jasurxabibullayev029@users.noreply.github.com>
2025-12-02 14:34:40 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 """
 WSGI config for skincare_ai project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
 import os
-import sys
-from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
-
-# Add the project directory to the Python path
-sys.path.insert(0, str(BASE_DIR))
-# Add the skincare_ai package to the path
-sys.path.insert(0, os.path.join(BASE_DIR, 'skincare_ai'))
-
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skincare.settings')
-
-# This application object is used by any WSGI server configured to use this file.
+import skincare_ai
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skincare_ai.settings')
+
 application = get_wsgi_application()
